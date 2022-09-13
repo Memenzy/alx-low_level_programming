fdd089c5dc1e1c4721e9b66d6b3f3af8fb31e0dc
Memenzy <mmormoni75@gmail.com>
2022-09-13 10:10:19 -0700

    Commit 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *
  * Return 0 (Sucess)
  */
-int main ()
+int main (void)
 {	
 	_putchar('_');
 	_putchar('p');
@@ -14,6 +14,7 @@ int main ()
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');	
+	_putchar('\n');
+
 	return 0;
 }

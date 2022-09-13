e5dea090934389b5f07711c644e9f4bce20f3242
Memenzy <mmormoni75@gmail.com>
2022-09-13 15:32:28 -0700

    1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#ifndef UNISTD_H
-#define UNISTD_H
-/**
- * _nutchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-#endif
+int _putchar(char c);

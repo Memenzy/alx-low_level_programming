09861426761e808d1e45e5803dfe64d7b06d582e
Memenzy <mmormoni75@gmail.com>
2022-09-13 15:47:07 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * main - Entry point 
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {

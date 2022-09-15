f00bd1e24a554f42fac77f3fc628804b9507a981
Memenzy <mmormoni75@gmail.com>
2022-09-15 07:33:36 -0700

    1

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include "main.h"
-/* more headers goes there */
+#include <stdlib.h>
+#include <time.h>
+/**
+ * positive_or_negative - this is the main code
+ * Return: null
+ */
 
-/* betty style doc for function main goes there */
-int main(void)
+void positive_or_negative(int i)
 {
-	int i;
-	
-	i = 98;
-	positive_or_negative(i);
-
-	return (0);
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is positive\n", i);
 }

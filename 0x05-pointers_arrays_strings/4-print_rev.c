ae206324e2b0e7bcd8060cf2e600d50a5cf80085
Memenzy <mmormoni75@gmail.com>
2022-09-19 15:47:57 -0700

    Task 4..

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, in reverse
+ * print_rev - a function that prints a string, in reverse.
  * @s: is a variable
  */
 void print_rev(char *s)

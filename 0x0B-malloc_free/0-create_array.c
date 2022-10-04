213a071128de24223682d609a88565851af498e5
Memenzy <mmormoni75@gmail.com>
2022-10-03 23:31:17 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * ceate_array - a function that creates an array of chars,
+ * create_array - a function that creates an array of chars,
  * and initializes it with a specific char
  * @size: size of the array
  * @c: specific character
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0 || array == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
 

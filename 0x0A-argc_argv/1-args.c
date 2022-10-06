428e52cab2e6def90a6780ff377763168546dc99
Memenzy <mmormoni75@gmail.com>
2022-10-05 17:53:41 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: arguements
+ * @argv: array pointing to arguemnts
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

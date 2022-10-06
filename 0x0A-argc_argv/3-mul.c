e0b88e5585977e0f48b3746bf8d602a7861a7c9a
Memenzy <mmormoni75@gmail.com>
2022-10-05 18:18:47 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: arguements
+ * @argv: array pointers to the arguements
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for(i =0; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
